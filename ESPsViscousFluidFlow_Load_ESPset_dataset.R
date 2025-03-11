664ba0b6429a3007b0d2fd62dd2654d2399f5692
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-11 11:37:41 -0300

    Update ESPsViscousFluidFlow_Load_ESPset_dataset.R

--- ESPsViscousFluidFlow_Load_ESPset_dataset.R
+++ ESPsViscousFluidFlow_Load_ESPset_dataset.R
@@ -6,7 +6,14 @@ project_folder="/home/felipe/googledrive/MachineLearningFaultDetectionESP/"    #
 project_folder="C:/Users/Valter/Documents/dataset/"                            # Without acces to the google driver
 
 # List all excel files
-file.list <- list.files(path =project_folder,patter='*.xlsx',full.names = TRUE)
+# Specify sheet by its name
+p100_water_1800_data <- data.frame(read_excel(paste(project_folder,"P100_water.xlsx",sep=""), sheet = "1800 rpm",skip = 8))
+p100_water_2400_data <- data.frame(read_excel(paste(project_folder,"P100_water.xlsx",sep=""), sheet = "2400 rpm",skip = 8))
+p100_water_3000_data <- data.frame(read_excel(paste(project_folder,"P100_water.xlsx",sep=""), sheet = "3000 rpm",skip = 8))
+p100_water_3500_data <- data.frame(read_excel(paste(project_folder,"P100_water.xlsx",sep=""), sheet = "3500 rpm",skip = 8))
+
+
+
 
 # Load all excel files in the list
 df.list <- lapply(file.list, read_excel)
