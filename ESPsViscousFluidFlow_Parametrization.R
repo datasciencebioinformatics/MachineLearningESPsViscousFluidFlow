8c3902cb1976992eb767ca1162970d519d2b4afe
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-28 16:08:15 -0300

    Create ESPsViscousFluidFlow_Parametrization.R

---
+++ ESPsViscousFluidFlow_Parametrization.R
@@ -0,0 +1,2 @@
+# The pump head is the hydraulic power or pump output power (PQ) transmitted to the fluid handled relative to ρ · g · Q. 
+# https://www.ksb.com/en-global/centrifugal-pump-lexicon/article/head-1115612
