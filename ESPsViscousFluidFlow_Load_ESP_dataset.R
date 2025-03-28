32e777db280eb89ce383c5c5aa6be012639165ed
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-28 08:21:41 -0300

    Update ESPsViscousFluidFlow_Load_ESP_dataset.R

--- ESPsViscousFluidFlow_Load_ESP_dataset.R
+++ ESPsViscousFluidFlow_Load_ESP_dataset.R
@@ -95,3 +95,21 @@ HC12500_water<-cbind(rbind(HC12500_water_1800_data[-1,],HC12500_water_2400_data[
 
 # Merge all water
 All_water<-rbind(P100_water,P47_water,P37_water,P62_water,HC10000_water,HC12500_water)
+#########################################################################################################
+# Path to metadata file
+metada_data_file="eps_metada.xlsx"
+
+# Load the metada file
+metada_data<-data.frame(read_excel(paste(project_folder,metada_data_file,sep=""),skip=1))
+
+# Split data.frame into RPM and rads
+metada_data_rpm <-cbind(metada_data[,c(1,2,3,4,5,6)],Metric="RPM")
+metada_data_rads<-cbind(metada_data[,c(1,2,7,8,9,10)],Metric="rads")
+
+# Split data.frame into RPM and rads
+colnames(metada_data_rpm)<-c("model","Impeller.diameter","1800","2400","3000","3500","Metric")
+colnames(metada_data_rads)<-c("model","Impeller.diameter","1800","2400","3000","3500","Metric")
+
+# Compile metada data
+metada_data<-rbind(metada_data_rpm,metada_data_rads)
+#########################################################################################################
