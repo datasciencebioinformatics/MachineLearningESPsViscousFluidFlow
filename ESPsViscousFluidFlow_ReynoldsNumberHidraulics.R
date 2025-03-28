6a947ce02c6bf8bde37b366d88b54700a631014e
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-03-28 11:26:59 -0300

    Update ESPsViscousFluidFlow_ReynoldsNumberHidraulics.R

--- ESPsViscousFluidFlow_ReynoldsNumberHidraulics.R
+++ ESPsViscousFluidFlow_ReynoldsNumberHidraulics.R
@@ -8,10 +8,15 @@ All_viscous$L <- metada_data[match(All_viscous$equip,metada_data$model),"Impelle
 # Mass flow rate can be used to calculate velocity through the volumetric flow rate equation. The equation is ṁ = (v * A) * ρ, where: 
 All_viscous$Q <-as.numeric(All_viscous$Flow.rate)/(3600*as.numeric(All_viscous$Inlet.Density.ρi))
 
+# Collumn to add the velocity calculated by the hydrolautics
+All_viscous$Velocity_H<-0
+
 # First, Calculate the velocity
 for (measure in rownames(All_viscous))
 {
-  velocity(D = na.omit(All_viscous)$L, Q = na.omit(All_viscous)$Q)
+  Q=All_viscous[measure,"Q"]
+  L=All_viscous[measure,"L"]
+  All_viscous[measure,"Velocity_H"]<-velocity(D = L, Q = Q)
 }
 #####################################################################################
 # d density    (kg/m3)                 ok
@@ -20,8 +25,17 @@ for (measure in rownames(All_viscous))
 # dv dynamic viscosity (Pa·s or kg/ms) cP*0.001
 # kv viscosity (m2/s)                  dv/d
 
-# Second, calculate the Reynolds number
+# Second, Calculate the reynolds number
 for (measure in rownames(All_viscous))
 {
+  Velocity_H = All_viscous[measure,"Velocity_H"]
+  L          =All_viscous[measure,"L"]
+
+  # https://en.wikipedia.org/wiki/Reynolds_number
+  vicosity=as.numeric(All_viscous[measure,"Outlet.Viscosity.mo"])
+  d=d                  # d density    (kg/m3)
+  dv=vicosity          # dv dynamic viscosity (Pa·s or kg/ms) cP*0.001
+  kv=dv/d              # kv kinetic viscosity (m2/s)) 
   
+  All_viscous[measure,"Re_H"]<-reynolds_number(V = Velocity_H, D = L, nu = kv)
 }
