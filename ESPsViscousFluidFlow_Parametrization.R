0a9d3e0ee247333d9ad57e3dad891a22ba7066b3
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-01 15:37:08 -0300

    Update ESPsViscousFluidFlow_Parametrization.R

--- ESPsViscousFluidFlow_Parametrization.R
+++ ESPsViscousFluidFlow_Parametrization.R
@@ -2,7 +2,15 @@
 # https://www.ksb.com/en-global/centrifugal-pump-lexicon/article/head-1115612
 # set  the gravitational constant
 # 9.81 meters per second squared (m/s2) is the approximate value of the acceleration due to gravity on Earth's surface. This value is represented by the letter g. 
-g=9.81
+g=9.81]
+# Store the useful power Ph
+All_viscous$P_h<-0
+
+# Store the pump head
+All_viscous$H<-0
+
+# Store the shaft torque is the mechanical parameter used to calculate the driving power or brake horsepower (BHP)
+All_viscous$BHP<-0
 
 # Store the flow rate in m3s1
 All_viscous$Q<-0
@@ -10,7 +18,7 @@ All_viscous$Q<-0
 # Store the diameter Impelleters diameter
 All_viscous$D<-0
 
-# Store the diameter Impelleters diameter
+# Store the kinetic viscosity
 All_viscous$kv<-0
 
 # First, Calculate the velocity
@@ -19,18 +27,24 @@ for (measure in rownames(All_viscous))
   # The Flow Rate, Q 
   # [kg/h]
   Q=as.numeric(All_viscous[measure,"Flow.rate"])
+
+  # Store inlet and outlet pressure
+  P1  <-as.numeric(All_viscous[measure,"Inlet.Pressure.P1"])
+  P2  <-as.numeric(All_viscous[measure,"Outlet.Pressure.P2"])
+
+  # Number of stages = 3
+  N   <-3 
   
   # Density
   # [kg/m³]
-  d  <-as.numeric(All_viscous[measure,"Inlet.Density.ρi"])
+  p  <-as.numeric(All_viscous[measure,"Inlet.Density.ρi"])
+
+  p*g*H*Q
 
   # Inner diameter of pipe, di in m
   # [m]
   id  <-as.numeric(metada_data[metada_data$model==equip & metada_data$Metric=="rads","Impeller.diameter"]) 
 
-  # Flow rate in m3/s 
-  # [m3/s]
-  All_viscous[measure,"Q"]<-Q/(d*3600)
 
   # [m]
   All_viscous[measure,"D"]<-id
