85ac845559b1a8d471abd5773883243e44095ce5
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-01 17:27:06 -0300

    Update ESPsViscousFluidFlow_Parametrization.R

--- ESPsViscousFluidFlow_Parametrization.R
+++ ESPsViscousFluidFlow_Parametrization.R
@@ -1,56 +1,74 @@
+# To do 01/04/2025
+# Check paper for the units of each variable
+# Reproduce figure 1. Flow rate (dimensoneless) vs. head 
+# Organize test matrixes
+
 # The pump head is the hydraulic power or pump output power (PQ) transmitted to the fluid handled relative to ρ · g · Q. 
 # https://www.ksb.com/en-global/centrifugal-pump-lexicon/article/head-1115612
 # set  the gravitational constant
 # 9.81 meters per second squared (m/s2) is the approximate value of the acceleration due to gravity on Earth's surface. This value is represented by the letter g. 
-g=9.81]
+g=9.81
 
 # Number of stages (constant equanto to 3)
 N   <-3 
 
 # Store the useful power Ph
-All_viscous$P_h<-0
+merge_water_viscous$P_h<-0
 
 # The pump efficiency (n) is defined as:
-All_viscous$n<-0
+merge_water_viscous$n<-0
 
 # Store the pump head
-All_viscous$H<-0
+merge_water_viscous$H<-0
 
 # Store the shaft torque is the mechanical parameter used to calculate the driving power or brake horsepower (BHP)
-All_viscous$BHP<-0
+merge_water_viscous$BHP<-0
 
 # Store the flow rate in m3s1
-All_viscous$Q<-0
+merge_water_viscous$Q<-0
+
+# dimensionless flow rate
+merge_water_viscous$teta<-0
 
 # First, Calculate the velocity
-for (measure in rownames(All_viscous))
-{                     
+for (measure in rownames(merge_water_viscous))
+{ 
   # The Flow Rate, Q 
   # [kg/h]
-  Q=as.numeric(All_viscous[measure,"Flow.rate"])
+  model=merge_water_viscous[measure,"equip"]
+
+  # Impeller Diameter
+  D=unique(metada_data[which(metada_data$model == merge_water_viscous[measure,"equip"]),2])
+  
+  # The Flow Rate, Q 
+  # [kg/h]
+  merge_water_viscous[measure,"Q"]<-as.numeric(merge_water_viscous[measure,"Flow.rate"])
   
   # Store inlet and outlet pressure
-  P1  <-as.numeric(All_viscous[measure,"Inlet.Pressure.P1"])
-  P2  <-as.numeric(All_viscous[measure,"Outlet.Pressure.P2"])
+  P1  <-as.numeric(merge_water_viscous[measure,"Inlet.Pressure.P1"])
+  P2  <-as.numeric(merge_water_viscous[measure,"Outlet.Pressure.P2"])
   
   # Density
-  p  <-as.numeric(All_viscous[measure,"Inlet.Density.ρi"])
+  p  <-as.numeric(merge_water_viscous[measure,"Inlet.Density.ρi"])
 
   # Head pump
-  All_viscous[measure,"H"]<-((P2-P1)/(d*g))*(1/N) 
+  merge_water_viscous[measure,"H"]<-((P2-P1)/(d*g))*(1/N) 
 
   # The rotational speed w in rpm
-  w=as.numeric(All_viscous[measure,"RPM"])
+  w=as.numeric(merge_water_viscous[measure,"RPM"])
 
   # Net.Shaft.Torque
-  T=as.numeric(All_viscous[measure,"Net.Shaft.Torque"])
+  T=as.numeric(merge_water_viscous[measure,"Shaft.Torque"])
 
   # ESP is the BHP
-  All_viscous[measure,"BHP"]=(1/N)*(w*T)
+  merge_water_viscous[measure,"BHP"]=(1/N)*(w*T)
 
   # useful power Ph
-  All_viscous[measure,"P_h"]<- p*g*All_viscous[measure,"H"]*Q
+  merge_water_viscous[measure,"P_h"]<- p*g*merge_water_viscous[measure,"H"]*Q
 
   # The pump efficiency (n) is defined as:
-  All_viscous[measure,"n"] <- All_viscous[measure,"P_h"]/BHP
+  merge_water_viscous[measure,"n"] <- merge_water_viscous[measure,"P_h"]/BHP
+
+  # dimensionless flow rate
+  merge_water_viscous[measure,"teta"]<- merge_water_viscous[measure,"Q"]/(w*(D^3))
 }
