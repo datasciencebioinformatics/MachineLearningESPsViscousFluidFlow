480e7d2e49008002a0344946061f81543d1481ed
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-02 14:27:34 -0500

    Update ESPsViscousFluidFlow_Parametrization.R

--- ESPsViscousFluidFlow_Parametrization.R
+++ ESPsViscousFluidFlow_Parametrization.R
@@ -88,7 +88,7 @@ ESP_P47_water<-merge_water_viscous[merge_water_viscous$RPM=="3500" & merge_water
 # Melt tabele
 ESP_P47_water_plot_Q_H <- ggplot(ESP_P47_water, aes(x = Q, y = H,shape = RPM,colour=fluid))   + geom_point() + theme_bw()   + ggtitle ("Flow rate Q (m3/h) vs. Head H")    + ylab("Head H")  +  xlab("Flow rate Q m3/h") + theme(legend.position = "none")
 ESP_P47_water_plot_BHP <- ggplot(ESP_P47_water, aes(x = Q, y = BHP,shape = RPM,colour=fluid))   + geom_point() + theme_bw() + ggtitle ("Flow rate Q (m3/h) vs. Power BHP") + ylab("Power BHP")  +  xlab("Flow rate Q m3/h")     + theme(legend.position = "none")
-ESP_P47_water_plot_n   <- ggplot(ESP_P47_water, aes(x = Q, y = n,shape = RPM,colour=fluid))   + geom_point() + theme_bw()   + ggtitle ("Flow rate Q (m3/h) vs. Efficiency n (%)") + ylab("Efficiency n (%)")  +  xlab("Flow rate Q m3/h")+ theme(legend.position = "bottom")
+ESP_P47_water_plot_n   <- ggplot(ESP_P47_water, aes(x = Q, y = n/100,shape = RPM,colour=fluid))   + geom_point() + theme_bw()   + ggtitle ("Flow rate Q (m3/h) vs. Efficiency n (%)") + ylab("Efficiency n (%)")  +  xlab("Flow rate Q m3/h")+ theme(legend.position = "bottom")
 
 # Melt tabele
 # Plot_raw_vibration_data.png                                                                                                            
