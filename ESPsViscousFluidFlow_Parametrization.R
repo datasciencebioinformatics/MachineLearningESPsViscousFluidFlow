0499fb6f993c435582bc980b344fda73fc56cd84
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-02 10:50:56 -0300

    Update ESPsViscousFluidFlow_Parametrization.R

--- ESPsViscousFluidFlow_Parametrization.R
+++ ESPsViscousFluidFlow_Parametrization.R
@@ -32,6 +32,9 @@ merge_water_viscous$Q<-0
 # dimensionless flow rate
 merge_water_viscous$teta<-0
 
+# dimensionless head psi
+merge_water_viscous$psi<-0
+
 # First, Calculate the velocity
 for (measure in rownames(merge_water_viscous))
 { 
@@ -84,4 +87,24 @@ for (measure in rownames(merge_water_viscous))
 
   # dimensionless flow rate
   merge_water_viscous[measure,"teta"]<- merge_water_viscous[measure,"Q"]/(w*(D^3))
+
+  # dimensionless head psi
+  merge_water_viscous[measure,"psi"]<- (merge_water_viscous[measure,"H"]*g)/((w^2)*(D^2))
 }
+################################################################################################################
+# Reproduce Fig. 5—ESP P47 dimensionless performance with water
+ESP_P47_water<-merge_water_viscous[merge_water_viscous$RPM=="3500" & merge_water_viscous$equip=="P47",]
+
+# Melt tabele
+ESP_P47_water_plot_Q_H <- ggplot(ESP_P47_water, aes(x = Q, y = H,shape = RPM,colour=fluid))   + geom_point() + theme_bw()   + ggtitle ("Flow rate Q (m3/h) vs. Head H")    + ylab("Head H (%)")  +  xlab("Flow rate Q m3/h") + theme(legend.position = "none")
+ESP_P47_water_plot_BHP <- ggplot(ESP_P47_water, aes(x = Q, y = BHP,shape = RPM,colour=fluid))   + geom_point() + theme_bw() + ggtitle ("Flow rate Q (m3/h) vs. Power BHP") + ylab("Power BHP")  +  xlab("Flow rate Q m3/h")     + theme(legend.position = "none")
+ESP_P47_water_plot_n   <- ggplot(ESP_P47_water, aes(x = Q, y = n,shape = RPM,colour=fluid))   + geom_point() + theme_bw()   + ggtitle ("Flow rate Q (m3/h) vs. Efficiency n (%)") + ylab("Efficiency n (%)")  +  xlab("Flow rate Q m3/h")+ theme(legend.position = "bottom")
+
+# Melt tabele
+
+# Plot_raw_vibration_data.png                                                                                                            
+png(filename=paste(project_folder,"ESP_P47_water_RPM3500.png",sep=""), width = 15, height = 20, res=600, units = "cm")  
+ grid.arrange(ESP_P47_water_plot_Q_H,ESP_P47_water_plot_BHP,ESP_P47_water_plot_n, nrow =3)
+dev.off()
+
+
