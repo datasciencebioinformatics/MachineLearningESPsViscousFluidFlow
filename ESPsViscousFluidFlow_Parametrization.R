b150d0dacddd2411f97d3da2bd584217dd3a7382
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-02 11:04:40 -0300

    Update ESPsViscousFluidFlow_Parametrization.R

--- ESPsViscousFluidFlow_Parametrization.R
+++ ESPsViscousFluidFlow_Parametrization.R
@@ -95,6 +95,7 @@ for (measure in rownames(merge_water_viscous))
 # Reproduce Fig. 5—ESP P47 dimensionless performance with water
 ESP_P47_water<-merge_water_viscous[merge_water_viscous$RPM=="3500" & merge_water_viscous$equip=="P47",]
 
+
 # Melt tabele
 ESP_P47_water_plot_Q_H <- ggplot(ESP_P47_water, aes(x = Q, y = H,shape = RPM,colour=fluid))   + geom_point() + theme_bw()   + ggtitle ("Flow rate Q (m3/h) vs. Head H")    + ylab("Head H")  +  xlab("Flow rate Q m3/h") + theme(legend.position = "none")
 ESP_P47_water_plot_BHP <- ggplot(ESP_P47_water, aes(x = Q, y = BHP,shape = RPM,colour=fluid))   + geom_point() + theme_bw() + ggtitle ("Flow rate Q (m3/h) vs. Power BHP") + ylab("Power BHP")  +  xlab("Flow rate Q m3/h")     + theme(legend.position = "none")
@@ -103,7 +104,7 @@ ESP_P47_water_plot_n   <- ggplot(ESP_P47_water, aes(x = Q, y = n,shape = RPM,col
 # Melt tabele
 
 # Plot_raw_vibration_data.png                                                                                                            
-png(filename=paste(project_folder,"ESP_P47_water_RPM3500.png",sep=""), width = 15, height = 20, res=600, units = "cm")  
+png(filename=paste(project_folder,"ESP_P47_water_RPM3500.png",sep=""), width = 20, height = 25, res=600, units = "cm")  
  grid.arrange(ESP_P47_water_plot_Q_H,ESP_P47_water_plot_BHP,ESP_P47_water_plot_n, nrow =3)
 dev.off()
 
