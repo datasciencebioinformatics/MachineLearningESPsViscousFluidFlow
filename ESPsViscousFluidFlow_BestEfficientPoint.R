87e8c339407d1a313eaf04edeb227cc9bd6d0f25
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-03 08:37:19 -0300

    Update ESPsViscousFluidFlow_BestEfficientPoint.R

--- ESPsViscousFluidFlow_BestEfficientPoint.R
+++ ESPsViscousFluidFlow_BestEfficientPoint.R
@@ -1,5 +1,5 @@
 # Fit a curve for Qxn
-fit <- lm(ESP_P47_water$Q ~ poly(ESP_P47_water$n, 3, raw=TRUE))
+fit <- lm(ESP_P47_water$Q ~ poly(ESP_P47_water$n, 5, raw=TRUE))
 
 # Find maximum value
 BEP_Q<-max(fit$fitted.values)
