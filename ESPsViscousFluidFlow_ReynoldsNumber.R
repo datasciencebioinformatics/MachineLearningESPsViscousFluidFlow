da0d43756455927a1844407fd992c2b43560a7ad
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-03 16:26:45 -0300

    Update ESPsViscousFluidFlow_ReynoldsNumber.R

--- ESPsViscousFluidFlow_ReynoldsNumber.R
+++ ESPsViscousFluidFlow_ReynoldsNumber.R
@@ -16,17 +16,22 @@ for (measure in rownames(All_viscous))
   p  <-as.numeric(All_viscous[measure,"Inlet.Density.ρi"])
 
   # Store viscosity
-  # [cP]
-  v<-as.numeric(All_viscous[measure,"Outlet.Viscosity.mo"])  
+  # [kg/ms] 
+  v<-as.numeric(All_viscous[measure,"Outlet.Viscosity.mo"]) *0.001
   
   # Inner diameter of pipe, di in m
   # [m]
   D2  <-as.numeric(metada_data[metada_data$model==equip & metada_data$Metric=="rads","Impeller.diameter"])*as.numeric(metada_data[metada_data$model==equip & metada_data$Metric=="rads","Impeller.diameter"])
 
   # The rotational speed w in rpm
-  # unit checked RPM
-  w=as.numeric(merge_water_viscous[measure,"RPM"])*0.10472
+  # unit checked RPM m/s
+  w = (2*pi*as.numeric(merge_water_viscous[measure,"RPM"]))/60
 
-  # Stor Reynollds number
-  All_viscous[,"Re"]= (p*w*D2)/v
+  # Store Reynollds number
+  All_viscous[measure ,"Re"]= (p*w*D2)/v
 }
+#####################################################################################
+png(filename=paste(project_folder,"Boxplot_All_viscous.png",sep=""), width = 15, height = 15, res=600, units = "cm")  
+  ggplot(na.omit(All_viscous), aes(x=fluid, y=Re_H, fill=equip)) +  geom_boxplot()+ theme(legend.position = "bottom") + theme_bw()
+dev.off()
+#####################################################################################
