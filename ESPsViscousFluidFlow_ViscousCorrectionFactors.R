d68527fc9d341318a970d3efe99fbf6acd1aca5f
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-03 08:45:28 -0300

    Update ESPsViscousFluidFlow_ViscousCorrectionFactors.R

--- ESPsViscousFluidFlow_ViscousCorrectionFactors.R
+++ ESPsViscousFluidFlow_ViscousCorrectionFactors.R
@@ -8,19 +8,16 @@ ESP_water<-na.omit(merge_water_viscous[merge_water_viscous$fluid=="water",])
 # Fluid samples 
 ESP_fluid<-na.omit(merge_water_viscous[merge_water_viscous$fluid!="water",])
 ################################################################################################################
-# The correction the BEP from the fitt curve Q*n.
+# First, take the BEP in water and in Viscous
 # Q (head) : (Q*n in water) AND (Q*n in viscous)
 
-# Fit a curve for Q_water_fit
-Q_fit <- lm(merge_water_viscous$Q ~ merge_water_viscous$n)
+# Fit a curve for Qxn
+Q_water_fit   <- lm(ESP_water$Q ~ poly(ESP_water$n, 5, raw=TRUE))
+Q_viscous_fit <- lm(ESP_fluid$Q ~ poly(ESP_fluid$n, 5, raw=TRUE))
 
 # Find maximum value
-BEP_Q<-max(Q_fit$fitted.values)
+BEP_water_Q<-max(Q_water_fit$fitted.values)
+BEP_viscous_Q<-max(Q_viscous_fit$fitted.values)
 ################################################################################################################
-
-# Fit a curve for Q_water_fit
-Q_water_fit <- lm(ESP_water$Q ~ ESP_water$Pi)
-
-# Fit a curve for Q_water_fit
-Q_viscous_fit <- lm(ESP_fluid$Q ~ ESP_fluid$Pi)
-
+# The BEP flow rate in water is analogous to the BEP flow rate in viscous fluids.
+# therefore, the BEP_water_Q is analogous BEP_water_Q.
