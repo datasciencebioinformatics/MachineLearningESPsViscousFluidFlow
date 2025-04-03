656006fae04d462c1af4bb5a00485c7378bcd564
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-03 07:44:46 -0300

    Update ESPsViscousFluidFlow_ViscousCorrectionFactors.R

--- ESPsViscousFluidFlow_ViscousCorrectionFactors.R
+++ ESPsViscousFluidFlow_ViscousCorrectionFactors.R
@@ -1,7 +1,18 @@
-# The correction factors for flow rate (CQ)
-# Take the flow rate from water samples
-merge_water_viscous[which(merge_water_viscous$fluid!="water"),"Q"]/merge_water_viscous[which(merge_water_viscous$fluid=="water"),"Q"]
- 
+################################################################################################################
+# Water samples
+ESP_water<-na.omit(merge_water_viscous[merge_water_viscous$fluid=="water",])
+
+# Fluid samples 
+ESP_fluid<-na.omit(merge_water_viscous[merge_water_viscous$fluid!="water",])
+################################################################################################################
+# The correction the BEP from the fitt curve Q*n.
+# Q (head) : (Q*n in water) AND (Q*n in viscous)
+
+# Fit a curve for Qxn
+Q_water_fit <- lm(ESP_water$Q ~ ESP_water$Pi)
+
+# Find maximum value
+Q_water_BEP<-max(Q_water_fit$fitted.values)
 
 # homologous conditions on the viscous and water curves
 
