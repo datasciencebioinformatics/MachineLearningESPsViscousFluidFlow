ce178a5dd237f675bec8e51827332b8e9ed2c8aa
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-07 10:54:28 -0300

    Update ESPsViscousFluidFlow_BEP_Caclulation.R

--- ESPsViscousFluidFlow_BEP_Caclulation.R
+++ ESPsViscousFluidFlow_BEP_Caclulation.R
@@ -1,3 +1,13 @@
+# Take unique values of viscosity, equipment and rpm
+unique_viscosity <-sort(unique(as.numeric(merge_water_viscous$Inlet.Viscosity)))
+unique_equipment <-sort(unique(merge_water_viscous$equip))
+unique_rpm <-sort(unique(merge_water_viscous$RPM))
+
+# Generate test matrix as data.frame
+expand.grid(unique_rpm,unique_equipment,unique_viscosity)
+
+# For each combination of test
+
 # The best efficient point will be calculated fro each configuration in the test matrix
 # Fit a curve for Qxn
 fit <- lm(ESP_P47_water$Q ~ poly(ESP_P47_water$n, 5, raw=TRUE))
