ca3595639170bb81e83447751498aff4c2ad8db3
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-08 14:34:23 -0300

    Update ESPsViscousFluidFlow_ReynoldsNumber.R

--- ESPsViscousFluidFlow_ReynoldsNumber.R
+++ ESPsViscousFluidFlow_ReynoldsNumber.R
@@ -1,23 +1,20 @@
 #########################################################################################################
 # Mass flow rate can be used to calculate velocity through the volumetric flow rate equation. The equation is ṁ = (v * A) * ρ, where: 
-All_viscous$Re <-0
-
-# v = m/(A*d)
-# m: Mass flow rate               [kg/h]
-# v: Velocity                     [m/h]
-# A: Cross-sectional area of flow [m2] 
-# d: Density                      [kg/m³]
+merge_water_viscous$Re <-0
 
 # First, Calculate the velocity
-for (measure in rownames(All_viscous))
+for (measure in rownames(merge_water_viscous))
 {
+  # Save the equipment 
+  equip=merge_water_viscous[measure,"equip"]
+  
   # Density
   # [kg/m³]
-  p  <-as.numeric(All_viscous[measure,"Inlet.Density.ρi"])
+  p  <-as.numeric(merge_water_viscous[measure,"Inlet.Density.ρi"])
 
   # Store viscosity
   # [kg/ms] 
-  v<-as.numeric(All_viscous[measure,"Outlet.Viscosity.mo"]) *0.001
+  v<-as.numeric(merge_water_viscous[measure,"Outlet.Viscosity.mo"])/p
   
   # Inner diameter of pipe, di in m
   # [m]
@@ -25,13 +22,13 @@ for (measure in rownames(All_viscous))
 
   # The rotational speed w in rpm
   # unit checked RPM m/s
-  w = (2*pi*as.numeric(merge_water_viscous[measure,"RPM"]))/60
+  w = as.numeric(merge_water_viscous[measure,"RPM"])
 
   # Store Reynollds number
-  All_viscous[measure ,"Re"]= (p*w*D2)/v
+  merge_water_viscous[measure ,"Re"]= (p*w*D2)/v
 }
 #####################################################################################
 png(filename=paste(project_folder,"Boxplot_All_viscous.png",sep=""), width = 15, height = 15, res=600, units = "cm")  
-  ggplot(na.omit(All_viscous), aes(x=fluid, y=Re_H, fill=equip)) +  geom_boxplot()+ theme(legend.position = "bottom") + theme_bw()
+  ggplot(na.omit(merge_water_viscous), aes(x=fluid, y=Re, fill=equip)) +  geom_boxplot()+ theme(legend.position = "bottom") + theme_bw()
 dev.off()
 #####################################################################################
