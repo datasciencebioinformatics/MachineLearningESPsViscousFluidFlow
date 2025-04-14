b2ebef1170d658614fa5df7c1600ff9ed5a4d545
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-14 13:54:52 -0300

    Update ESPsViscousFluidFlow_EmpiricalSimulation.R

--- ESPsViscousFluidFlow_EmpiricalSimulation.R
+++ ESPsViscousFluidFlow_EmpiricalSimulation.R
@@ -1,12 +1,15 @@
-- Correction coffecients
-- Coeficients in fluid versus. coefficients in water
+# TO DO : Calculate correlation coefficientes.
+# Study how to replace for machine learning models.
 
-- Empirical studies  : simulate changes in the values of viscusity 
-- CQ vs. Viscosities : Obtain the values Q along the categories of viscosities
-- CH vs. Viscosities : Obtain the values of H along the categories of viscosity
-- CN vs. Viscosities : Obtain the values of N along the categories of viuscosity
+# Correction coffecients
+# Coeficients in fluid versus. coefficients in water
 
-- Empirical studies  : simulate changes in the values of Rotational Reynoalds
-- CQ vs. Rotational Reynoalds: Obtain the values Q along the categories of Rotational Reynoalds
-- CH vs. Rotational Reynoalds: Obtain the values of H along the categories of Rotational Reynoalds
-- CN vs. Rotational Reynoalds: Obtain the values of N along the categories of Rotational Reynoalds
+# Empirical studies  : simulate changes in the values of viscusity 
+# CQ vs. Viscosities : Obtain the values Q along the categories of viscosities
+# CH vs. Viscosities : Obtain the values of H along the categories of viscosity
+# CN vs. Viscosities : Obtain the values of N along the categories of viuscosity
+
+# Empirical studies  : simulate changes in the values of Rotational Reynoalds
+# CQ vs. Rotational Reynoalds: Obtain the values Q along the categories of Rotational Reynoalds
+# CH vs. Rotational Reynoalds: Obtain the values of H along the categories of Rotational Reynoalds
+# CN vs. Rotational Reynoalds: Obtain the values of N along the categories of Rotational Reynoalds
