186a355b830de25486d62de0f2bc3100973ddc3f
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-14 13:53:54 -0300

    Create ESPsViscousFluidFlow_EmpiricalSimulation.R

---
+++ ESPsViscousFluidFlow_EmpiricalSimulation.R
@@ -0,0 +1,12 @@
+- Correction coffecients
+- Coeficients in fluid versus. coefficients in water
+
+- Empirical studies  : simulate changes in the values of viscusity 
+- CQ vs. Viscosities : Obtain the values Q along the categories of viscosities
+- CH vs. Viscosities : Obtain the values of H along the categories of viscosity
+- CN vs. Viscosities : Obtain the values of N along the categories of viuscosity
+
+- Empirical studies  : simulate changes in the values of Rotational Reynoalds
+- CQ vs. Rotational Reynoalds: Obtain the values Q along the categories of Rotational Reynoalds
+- CH vs. Rotational Reynoalds: Obtain the values of H along the categories of Rotational Reynoalds
+- CN vs. Rotational Reynoalds: Obtain the values of N along the categories of Rotational Reynoalds
