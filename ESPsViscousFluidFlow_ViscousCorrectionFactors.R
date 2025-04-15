40b4dfb57cfa0cb77b5bc5ecd6116c53f01da9ec
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-15 09:56:03 -0300

    Update ESPsViscousFluidFlow_ViscousCorrectionFactors.R

--- ESPsViscousFluidFlow_ViscousCorrectionFactors.R
+++ ESPsViscousFluidFlow_ViscousCorrectionFactors.R
@@ -1,12 +1,23 @@
 ################################################################################################################
-# All samples Water samples
-merge_water_viscous
+# Needed variables
+# Q    : The flow capacity in cubic meters per hour (m3/h)                    OK
+# p    : The density of the fluid in kilograms per cubic meter (kg/m3)        OK (must be calculated for water)
+# g    : The acceleration due to gravity (9.81m/s2)                           OK
+# H    : The differential head in meters (m)                                  OK
+# T    : Net.Shaft.Torque                                                     OK
+# N    : Number of stage                                                      OK
+################################################################################################################
+# Viscous samples
+# All_viscous
 
 # Water samples
-ESP_water<-na.omit(merge_water_viscous[merge_water_viscous$fluid=="water",])
+# All_water
 
-# Fluid samples 
-ESP_fluid<-na.omit(merge_water_viscous[merge_water_viscous$fluid!="water",])
+# Make the colnames correspondents
+################################################################################################################
+# For each viscous category o of the test matrix, calculate the BEP_water_Q and BEP_viscous_Q 
+# For each test condition, the head, BHP and efficiency are already calculate.
+# TO DO : use the values at the calculated BEP_water_Q and BEP_viscous_Q
 ################################################################################################################
 # First, take the BEP in water and in Viscous
 # Q (head) : (Q*n in water) AND (Q*n in viscous)
@@ -24,49 +35,6 @@ BEP_viscous_Q<-max(Q_viscous_fit$fitted.values)
 ESP_water_BEP_Q<-ESP_water[ESP_water$Q == sort((ESP_water)$Q)[match.closest(BEP_water_Q,  sort((ESP_water)$Q))],]
 ESP_fluid_BEP_Q<-ESP_fluid[ESP_fluid$Q == sort((ESP_fluid)$Q)[match.closest(BEP_viscous_Q,  sort((ESP_fluid)$Q))],]
   
-# P = pressure in water
-P1_water  <-as.numeric(ESP_water_BEP_Q[,"Inlet.Pressure.P1"])  * 100000 # Bar converted to pa
-P2_water  <-as.numeric(ESP_water_BEP_Q[,"Outlet.Pressure.P2"]) * 100000 # Bar converted to pa
-
-# P = pressure in viscous
-P1_viscous  <-as.numeric(ESP_fluid_BEP_Q[,"Inlet.Pressure.P1"])  * 100000 # Bar converted to pa
-P2_viscous  <-as.numeric(ESP_fluid_BEP_Q[,"Outlet.Pressure.P2"]) * 100000 # Bar converted to pa
-
-# Density in water and in viscous
-# p = density, mL–3, kg/m3
-# Unit checked kg/m3
-p_water    <-as.numeric(ESP_water_BEP_Q[,"Inlet.Density.ρi"])
-p_viscous  <-as.numeric(ESP_fluid_BEP_Q[,"Inlet.Density.ρi"])
-
-# Head pump
-# H = head, L, m
-ESP_water_BEP_Q[,"H"]<-((P2_water-P1_water)/(p_water*g))*(1/N)
-ESP_fluid_BEP_Q[,"H"]<-((P2_viscous-P1_viscous)/(p_viscous*g))*(1/N)
-
-# The rotational speed w in rpm
-# unit checked RPM
-w_water  =as.numeric(ESP_water_BEP_Q[,"RPM"])*0.10472
-w_viscous=as.numeric(ESP_fluid_BEP_Q[,"RPM"])*0.10472
-
-# Net.Shaft.Torque
-# T = shaft torque, mL2,t–2, N·m
-# unit checked N·m
-T_water=as.numeric(ESP_water_BEP_Q[,"Shaft.Torque"])
-T_viscous=as.numeric(ESP_fluid_BEP_Q[,"Shaft.Torque"])
-
-# ESP is the BHP
-# BHP = mL^2t^–3, watts
-ESP_water_BEP_Q[,"BHP"]=(1/N)*(w_water*T_water)
-ESP_fluid_BEP_Q[,"BHP"]=(1/N)*(w_viscous*T_viscous)
-
-# useful power Ph
-ESP_water_BEP_Q[,"P_h"]<- p_water*g*ESP_water_BEP_Q[,"H"]*ESP_water_BEP_Q[,"Q"]
-ESP_fluid_BEP_Q[,"P_h"]<- p_viscous*g*ESP_fluid_BEP_Q[,"H"]*ESP_fluid_BEP_Q[,"Q"]
-
-# The pump efficiency (n) is defined as:
-# n = efficiency, dimensionless [%]
-ESP_water_BEP_Q[,"n"] <- ESP_water_BEP_Q[,"P_h"]/ESP_water_BEP_Q[,"BHP"]
-ESP_fluid_BEP_Q[,"n"] <- ESP_fluid_BEP_Q[,"P_h"]/ESP_fluid_BEP_Q[,"BHP"]
 ######################################################################################3
 # The BEP flow rate in water is analogous to the BEP flow rate in viscous fluids.
 # therefore, the BEP_water_Q is analogous BEP_water_Q.
@@ -77,8 +45,3 @@ C_n=ESP_water_BEP_Q[,"n"]/ESP_fluid_BEP_Q[,"n"]
 
 # Correction 
 C_H=ESP_water_BEP_Q[,"H"]/ESP_fluid_BEP_Q[,"H"]
-
-
-
-
-
