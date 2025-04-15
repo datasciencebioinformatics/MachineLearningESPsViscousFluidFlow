8a1ac11db1004e3e23af3db09f7d89842ab2ecd1
datasciencebioinformatics <57751987+datasciencebioinformatics@users.noreply.github.com>
2025-04-15 10:40:42 -0300

    Update ESPsViscousFluidFlow_Load_ESP_dataset.R

--- ESPsViscousFluidFlow_Load_ESP_dataset.R
+++ ESPsViscousFluidFlow_Load_ESP_dataset.R
@@ -113,10 +113,10 @@ metada_data<-rbind(metada_data_rpm,metada_data_rads)
 colnames(All_viscous)[9]<-"Shaft.Torque"
 
 # Calculate water density at inlet temperature
-t = as.numeric(All_water$Inlet.Temperature.T1)
-# p = (999.83952 + 16.945176*(t) - 7.9870401*(10^-3)*(t^2) - 46.170461*(10^-6)*(t^3) + 105.56302*(10^-9)*(t^4) - 280.54253 * (10^-12)*(t^5)) / (1 + 16.897850 * (10^-3)*(t))
-p  = dens(T = as.numeric(All_water$Inlet.Temperature.T1), units = 'SI')
-mu = dvisc(T = as.numeric(All_water$Inlet.Temperature.T1), units = 'SI')*1000
+p  = water("rho", T = convert(as.numeric(All_water$Inlet.Temperature.T1), "K"), P = as.numeric(All_water$Inlet.Pressure.P1))
+
+# Implement equation to calculate viscosity
+mu = dvisc(T = as.numeric(All_water$Inlet.Temperature.T1), units = 'SI')*10000
 
 # Add collumns for viscosities in water. 
 # Star values as -1
